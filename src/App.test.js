653e4467c46a69fc4cc0aba8237411da23cd08dc
Harini Pasupathi <pasupathiharini5224@gmail.com>
2025-06-10 00:42:00 +0530

    Restore lost test files

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,5 @@
 // src/App.test.js
+<<<<<<< HEAD
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';  // <--- Add this line
@@ -8,4 +9,13 @@ test('renders task manager heading', () => {
   render(<App />);
   const headingElement = screen.getByText(/task manager/i);
   expect(headingElement).toBeInTheDocument();
+=======
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+>>>>>>> 2448a22e02f0d2966b34800ad60775554a427c84
 });
